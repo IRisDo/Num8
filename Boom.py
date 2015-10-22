b41e440d72743309e7b8e1ada2cf935ce9345b2a
IRisDo <shichibukai.sf@gmail.com>
2015-10-22 11:36:35 +0700

    Boom

---
+++ Boom.py
@@ -0,0 +1,33 @@
+# Tao matran
+L =[[0,0,0,0,0,0,0,0,0,0]]
+for k in range(1,11):
+    L.append([0,0,0,0,0,0,0,0,0,0])
+    
+# Cach danh so
+print("Co 10x10 o, gom 9 hang va 9 cot, cot va hang dau tien duoc danh so 0 !\n")
+
+# Nhap vi tri dat boom
+i,j = 0,0
+while i != -1 and j != -1:
+    i,j = int(input("Nhap vao vi tri dat boom va ket thuc bang -1 -1, i j = \n")),int(input())
+    L[i][j] = 1
+    
+# Giai thuat de qui
+def Boom(i,j):
+    while i <= 10 and j <= 10 and L[i][j] != 0: # Dieu kien de (i,j) khong vuot qua 10x10 va vi tri (i,j) co bom
+        L[i][j] = 0                             # Bom no
+        print('(',i,' ',j,')')                  # in ra vi tri no theo thu tu truoc sau
+        Boom(abs(i+1),abs(j))                   # xet cac o ke ben
+        Boom(abs(i),abs(j-1))
+        Boom(abs(i-1),abs(j))
+        Boom(abs(i),abs(j+1))
+        
+                    
+# Phan chuong trinh chinh
+if __name__ == '__main__':
+    i,j = int(input("Nhap vao cac vi tri cho no dau tien boom: i j = \n")),int(input())
+    if L[i][j] == 1:
+        Boom(i,j)
+    else:
+        print("Khong co vu no nao xay ra")
+        
